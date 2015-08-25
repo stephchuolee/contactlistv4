13881af325e872ca3bbcca9a3dc7d6d60c6412d3
schlorantics <schuolee@gmail.com>
2015-08-25 18:06:39 +0000

    Initial Commit - Sinatra and Routes Setup

---
+++ app/actions.rb
@@ -0,0 +1,28 @@
+# Homepage (Root path)
+get '/' do
+  erb :index
+end
+
+get '/contacts' do
+end 
+
+get '/contacts/new' do 
+end 
+
+post '/contacts/new' do
+end
+
+get '/contacts/:id' do
+end 
+
+get '/contacts/:id/edit' do
+end 
+
+put '/contacts/:id/edit' do
+end 
+
+get '/contacts/search' do
+end
+
+delete '/contacts/:id' do
+end 
